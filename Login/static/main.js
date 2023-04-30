8491dbe571fbe9f94b582c6e7ee3eeaddf570bd2
Visionario6 <111612564+Visionario6@users.noreply.github.com>
2023-04-30 18:22:41 -0500

    Add files via upload

---
+++ Login/static/main.js
@@ -0,0 +1,28 @@
+const dtop = {
+    pageLength: 50,
+    destroy:true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    }
+
+};
+
+
+window.$('#datatable_pacientes').DataTable(dtop);
+
+
+
+
+
