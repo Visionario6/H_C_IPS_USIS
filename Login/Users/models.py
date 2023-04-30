4f691cf3ad75b80040bae8eb03b52129a20ae687
Visionario6 <111612564+Visionario6@users.noreply.github.com>
2023-04-30 18:22:06 -0500

    Add files via upload

--- Login/Users/models.py
+++ Login/Users/models.py
@@ -1,13 +1,23 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, AbstractBaseUser, PermissionsMixin
+from Core.models import *
 
 # Create your models here.
-class User(AbstractUser):
+class User(AbstractUser, AbstractBaseUser, PermissionsMixin):
     ROL_CHOICES = [
         ('PA', 'PACIENTE'),
         ('ES', 'ESPECIALISTA')
     ]
-    cedula = models.CharField(max_length=11, blank=False, help_text=(
+    cedula = models.CharField(max_length=11, verbose_name='Cédula', blank=False, help_text=(
             "Requerido. Únicamente números. Sin puntos ni comas"
         ))
-    rol = models.CharField(max_length=2, choices=ROL_CHOICES, default='PA')
+    is_especialista = models.BooleanField(
+        ("Especialista status"),
+        default=False
+    )
+
+    def Nombreuser(self):
+        return "{} {} - {}".format(self.first_name,self.last_name,self.cedula)
+
+    def __str__(self):
+        return self.Nombreuser()
